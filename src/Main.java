a577129bb65f1d8ec06f5fc56aa27a7040fbc59b
KSHATest <sklyzet@yandex.ru>
2024-02-23 11:21:53 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int schet = 1000;
-        if (schet >= 1000) {
-            int balance = (schet / 100);
-            int bonus = (schet + balance);
+        int money = 1000;
+        if (money >= 1000) {
+            int balance = (money / 100);
+            int bonus = (money + balance);
             System.out.println("Итоговый счет:" + bonus);
             System.out.println("Бонусные рубли:" + balance);
         } else {
             int balance = (0);
-            System.out.println(schet);
+            System.out.println(money);
             System.out.println("Бонусные рубли:" + balance);
         }
     }
